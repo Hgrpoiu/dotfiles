72c0fa73d20ea01aa7b315a241079a4f9d7d645c
JQM <hgrpoiu@gmail.com>
2022-09-15 08:55:41 -0500

    Added command to auto install Vundle

--- firstConfig.sh
+++ firstConfig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/VundleVim/Vundle.vim.git "$PWD/../.vim/bundle/Vundle.vim"
+
 for dir in .[^.]*; do
 	sudo rm -r $PWD/../$dir
 	ln -s "$PWD/$dir" "$PWD/../$dir"
